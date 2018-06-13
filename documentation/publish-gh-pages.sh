b4722648941c791bfafe9680b9032bab74f7a49c
Katharina <katharina@everymundo.com>
2018-06-13 16:01:10 -0400

    updates publishing script

--- documentation/publish-gh-pages.sh
+++ documentation/publish-gh-pages.sh
@@ -1,10 +1,14 @@
+set -e
 set -x
 cd $(dirname $0)
+git checkout master
+git pull origin master
+git push origin master
 npm run build
 git add .
-git commit -m 'new documentation automatic generated'
+git commit -m 'new documentation automatically generated'
 git checkout gh-pages
 git pull origin gh-pages
-git merge master
+git rebase master
 git push origin gh-pages
 git checkout master
