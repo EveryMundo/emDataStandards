c066fc0cec51d9e48c5be96fdd4e393af4af8e47
Katharina <katharina@everymundo.com>
2018-06-13 16:17:45 -0400

    new documentation automatically generated

--- documentation/publish-gh-pages.sh
+++ documentation/publish-gh-pages.sh
@@ -4,7 +4,11 @@ cd $(dirname $0)
 git checkout master
 git pull origin master
 git push origin master
-npm run build
+# npm run build
+  npm run clean
+  export NODE_ENV=production
+  node_modules/.bin/webpack --config webpack/config.prod.withjs.babel.js
+  rm build/static-*
 git add .
 git commit -m 'new documentation automatically generated'
 git checkout gh-pages
