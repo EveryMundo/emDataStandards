bb03d5075ffecd0e9b34275b64c0548a17a33848
Ruben Alanis <49915647+rubenalanis@users.noreply.github.com>
2021-10-19 18:03:03 -0400

    Update modulemanager.datalayer.js

--- dataLayer/modulemanager.datalayer.js
+++ dataLayer/modulemanager.datalayer.js
@@ -2,26 +2,36 @@ MMdataLayer = [{
     "languageIsoCode": "en",
     "countryIsoCode ": "US",
     "originLocations ": [
-      "MIA","TXL","SXF"
+        "MIA", "TXL", "SXF"
     ],
     "destinationLocations": [
-      "LHR","MIL","LAX"
+        "LHR", "MIL", "LAX"
     ],
     "productCategory": "ONE_WAY",
     "productType": "ECONOMY",
-    "loyaltyProgram":{
-      "isLoggedIn": true,
-      "membershipLevel": "Basic",
-      "redemptionUnit": "MILES",
-      "redemptionBalance": 2500,
-      "lastSearchedProducts": [
-        "MIA>JFK"
-      ],
-      "homeLocation": "MIA",
-      "savedProducts": [
-        "NYC>CHI","ORD>MIA"
-      ],
+    "loyaltyProgram": {
+        "isLoggedIn": true,
+        "membershipLevel": "Basic",
+        "redemptionUnit": "MILES",
+        "redemptionBalance": 2500,
+        "lastSearchedProducts": [
+            "MIA>JFK"
+        ]
+    },
+    "homeLocation": "MIA",
+    "savedProducts": [
+        "NYC>CHI", "ORD>MIA"
+    ],
+    "experience": [
+        "type": "Ticket",
+        "name": "Concert"
+    ],
+    "event": [
+        "location": "Margaret Court Arena",
+        "date": "Day 1",
+        "session": "Night",
+        "time": "11:00 AM ET"
+    ],
     "preferredProductCategory": "ROUND_TRIP",
     "preferredProductType": "BUSINESS"
-    }
-  }];
+}];
