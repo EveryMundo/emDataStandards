a57646c1ecca40d89a10c613fd8e18ac3168ef13
Ruben Alanis <49915647+rubenalanis@users.noreply.github.com>
2021-10-25 10:54:58 -0400

    Update modulemanager.datalayer.js

--- dataLayer/modulemanager.datalayer.js
+++ dataLayer/modulemanager.datalayer.js
@@ -2,26 +2,39 @@ MMdataLayer = [{
     "languageIsoCode": "en",
     "countryIsoCode ": "US",
     "originLocations ": [
-      "MIA","TXL","SXF"
+        "MIA", "TXL", "SXF"
     ],
     "destinationLocations": [
-      "LHR","MIL","LAX"
+        "LHR", "MIL", "LAX"
     ],
     "productCategory": "ONE_WAY",
     "productType": "ECONOMY",
-    "loyaltyProgram":{
-      "isLoggedIn": true,
-      "membershipLevel": "Basic",
-      "redemptionUnit": "MILES",
-      "redemptionBalance": 2500,
-      "lastSearchedProducts": [
+    "homeLocation": "MIA",
+    "budgetMaximumAmount": "15000"
+    "budgetRedemptionUnit": "MILES",
+    "experience": [
+        "type": ["Ticket Only", "Dining"]
+        "name": ["Rod Laver Arena Reserved Seat", "Penfolds Restaurant"]
+    ],
+    "event": [
+        "location": ["Margaret Court Arena", "Rod Laver Arena"]
+        "date": [1, 2, 3, 4, 14],
+        "session": ["Night", "Day", "Twilight"],
+        "time": ["11:00 AM", "8:00 PM"]
+    ],
+    "experience": [
+        "type": "Ticket",
+        "name": "Concert"
+    ],
+    "loyaltyProgram": [
+        "isLoggedIn": true,
+        "membershipLevel": "Basic",
+        "redemptionUnit": "MILES",
+        "redemptionBalance": 2500,
+    ],
+    "lastSearchedProducts": [
         "MIA>JFK"
-      ],
-      "homeLocation": "MIA",
-      "savedProducts": [
-        "NYC>CHI","ORD>MIA"
-      ],
+    ],
     "preferredProductCategory": "ROUND_TRIP",
     "preferredProductType": "BUSINESS"
-    }
-  }];
+}]
