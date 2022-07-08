c660e54ed9ecd795b3f6e34a36edaa9ee2979484
Ruben Alanis <49915647+rubenalanis@users.noreply.github.com>
2022-07-08 00:02:57 -0400

    Create em.ecommerce.dataLayer.js

---
+++ dataLayer/em.ecommerce.dataLayer.js
@@ -0,0 +1,62 @@
+dataLayer.push({ ecommerce: null });
+dataLayer.push({
+  event: "purchase",
+  ecommerce: {
+      transaction_id: "ZTSRTM",
+      affiliation: "iflyea.com",
+      value: 599.89,
+      tax: 40.90,
+      shipping: 0,
+      currency: "EUR",
+      coupon: "AFFBFAN",
+      payment_method: "American Express",
+      flight_days_in_advance: "50",
+      flight_departure_date: "2021-11-14",
+      flight_return_date: "2021-11-30",
+      total_pax: 2,
+      total_adult: 1,
+      total_child: 1,
+      total_infant: 0,
+      flight_trip: "MIA-MVD",
+      flight_type: "RT",
+      user_id: "731108519",
+      FFP_logged_in: "TRUE",
+      FFP_loyalty_level: "TRUE",
+      FFP_redeemed_miles: 0,
+      items: [{
+        item_name: "MIA-MVD",
+        item_id: "Flight",
+        price: 300.50,
+        item_brand: "Pathfinder",
+        item_category: "2D777",
+        item_variant: "2021-11-14",
+        quantity: 2
+      }, {
+        item_name: "MVD-MIA",
+        item_id: "Flight",
+        price: 299.00,
+        item_brand: "Pathfinder",
+        item_category: "2D717",
+        item_variant: "2021-11-30",
+        quantity: 2
+      }, {
+        item_name: "Seat",
+        item_id: "Seat",
+        price: 20.25,
+        item_category: "Preferred",
+        quantity: 1
+      }, {
+        item_name: "Baggage",
+        item_id: "Baggage",
+        price: 15.25,
+        item_category: "2D777",
+        quantity: 1
+      }, {
+        item_name: "Refund",
+        item_id: "Refund",
+        price: 33.75,
+        item_category: "RFND",
+        quantity: 1
+      }]
+  }
+});
