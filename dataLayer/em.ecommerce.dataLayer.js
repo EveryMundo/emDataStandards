59fca7dcab325f4e6a7593bb1f40a2440dba6cce
Ruben Alanis <49915647+rubenalanis@users.noreply.github.com>
2022-08-18 04:36:35 -0400

    Update em.ecommerce.dataLayer.js

--- dataLayer/em.ecommerce.dataLayer.js
+++ dataLayer/em.ecommerce.dataLayer.js
@@ -1,62 +1,191 @@
-dataLayer.push({ ecommerce: null });
-dataLayer.push({
-  event: "purchase",
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+  event: 'purchase',
+  dataLayer_version: '1',
+  workflow_entry_point: 'Confirmation Page', //Confirmation Page, Manage My Booking, Web Check-In
+  payment_method: 'American Express',
+  flight_days_in_advance: '50',
+  flight_departure_date: '2021-11-14',
+  flight_return_date: '2021-11-30',
+  total_pax: 2,
+  total_adult: 1,
+  total_child: 1,
+  total_infant: 0,
+  flight_trip: 'MIA>MVD',
+  flight_type: 'ROUND_TRIP',
+  total_ancillary: '7', //Total of distinct selected ancillaries -> item_id (Does not include Flights)
+  additional_fees: 115.07, //Anything that the airline changes extra that is not taxes. For example: Airport fees
+  redemption_unit: 'MILES', //Miles, points
+  redemption_category: 'AdvantageProgram', //Name of the program
+  total_redemption_amount: 15000,
+  site_edition: 'en-US',
+  user_id: '731108519', //Loyalty program user ID -> Cross-device Tracking (Optional)
   ecommerce: {
-      transaction_id: "ZTSRTM",
-      affiliation: "iflyea.com",
-      value: 599.89,
-      tax: 40.90,
-      shipping: 0,
-      currency: "EUR",
-      coupon: "AFFBFAN",
-      payment_method: "American Express",
-      flight_days_in_advance: "50",
-      flight_departure_date: "2021-11-14",
-      flight_return_date: "2021-11-30",
-      total_pax: 2,
-      total_adult: 1,
-      total_child: 1,
-      total_infant: 0,
-      flight_trip: "MIA-MVD",
-      flight_type: "RT",
-      user_id: "731108519",
-      FFP_logged_in: "TRUE",
-      FFP_loyalty_level: "TRUE",
-      FFP_redeemed_miles: 0,
-      items: [{
-        item_name: "MIA-MVD",
-        item_id: "Flight",
-        price: 300.50,
-        item_brand: "Pathfinder",
-        item_category: "2D777",
-        item_variant: "2021-11-14",
-        quantity: 2
-      }, {
-        item_name: "MVD-MIA",
-        item_id: "Flight",
-        price: 299.00,
-        item_brand: "Pathfinder",
-        item_category: "2D717",
-        item_variant: "2021-11-30",
-        quantity: 2
-      }, {
-        item_name: "Seat",
-        item_id: "Seat",
-        price: 20.25,
-        item_category: "Preferred",
-        quantity: 1
-      }, {
-        item_name: "Baggage",
-        item_id: "Baggage",
-        price: 15.25,
-        item_category: "2D777",
-        quantity: 1
-      }, {
-        item_name: "Refund",
-        item_id: "Refund",
-        price: 33.75,
-        item_category: "RFND",
-        quantity: 1
-      }]
+    transaction_id: 'RBN794',
+    affiliation: 'airline.com', //Airline hostname
+    value: 599.89,
+    tax: 40.90,
+    currency: 'EUR',
+    coupon: 'AFFBFAN',
+    items: [{
+      item_name: 'MIA>MVD',
+      item_id: 'FLIGHT',
+      price: 300.50,
+      item_category: 'First Class', //Fareclass Category
+      item_category2: '2021-11-14', //Flight departure date
+      item_category3: 'AA477', //Flight number(s)
+      item_category4: 'PT3H1M', //Estimated flight duration
+      item_category5: 'Adult', //Passenger type
+      item_variant: 'MIA>JFK>MVD', //Full route
+      quantity: 2
+    }, {
+      item_name: 'MVD>MIA',
+      item_id: 'FLIGHT',
+      price: 299.00,
+      item_category: 'Business Class',
+      item_category2: '2021-11-30', //Flight departure date
+      item_category3: 'AA901', //Flight number(s)
+      item_category4: 'PT3H1M', //Estimated flight duration
+      item_category5: 'Adult', //Passenger type
+      item_variant: 'MVD>JFK>MIA', //Full route
+      quantity: 2
+    }, {
+      item_name: 'First Class', //Lie Flat, Business Class, First Class, Extra Leg Room, Front of Aircraft, Economy
+      item_id: 'SEAT',
+      price: 20.25,
+      item_category: 'Seat',
+      item_list_id: 'SEAT',
+      quantity: 1
+    }, {
+      item_name: 'Hyatt Miami', //Hotel Name
+      item_id: 'HOTEL',
+      price: 15.25,
+      item_category: 'Suite',
+      quantity: 1
+    }, {
+      item_name: 'Apartment', //Apartment, House, Farm
+      item_id: 'PROPERTY RENTAL',
+      price: 15.25,
+      item_category: 'Marquis Residencies',
+      quantity: 1
+    }, {
+      item_name: 'Hertz', //Company Name
+      item_id: 'CAR',
+      price: 15.25,
+      item_category: 'BMW', //Car Model
+      quantity: 1
+    }, {
+      item_name: 'Pricess', //Company Name
+      item_id: 'CRUISE',
+      price: 15.25,
+      item_category: 'Cruise',
+      quantity: 1
+    }, {
+      item_name: 'Checked Bag', //Carry-On, Checked, Overweight, Oversized, Sporting Equipment, Hands-Free, Excess Baggage
+      item_id: 'BAGGAGE',
+      price: 15.25,
+      item_category: '23kg',
+      item_list_id: 'PDBG',
+      quantity: 1
+    }, {
+      item_name: 'Flight Insurance', //Flight, Lost Baggage, Valuable Bag
+      item_id: 'INSURANCE',
+      price: 15.25,
+      item_category: 'Insurance',
+      quantity: 1
+    }, {
+      item_name: 'Miles Purchase', //Paid Membershit, Miles Purchase
+      item_id: 'LOYALTY PROGRAM',
+      price: 15.25,
+      item_category: '5000', //Membership level, Miles Purchased
+      item_list_id: 'PDFF',
+      quantity: 1
+    }, {
+      item_name: 'Fast Track Security', //Priority Boarding, Priority Security, Priority Bag Drop, Priority Check-in, Priority Bag Return
+      item_id: 'PIORITY SERVICE',
+      price: 15.25,
+      item_category: 'Priority Boarding',
+      item_list_id: 'PDFF',
+      quantity: 1
+    }, {
+      item_name: 'Wheel Chair',
+      item_id: 'MEDICAL',
+      price: 15.25,
+      item_category: 'Medical Services',
+      item_list_id: 'WCBD',
+      quantity: 1
+    }, {
+      item_name: 'Snorkeling', //Name of the Activity, Concert, Tour
+      item_id: 'ACTIVITY',
+      price: 15.25,
+      item_category: 'Activity',
+      quantity: 1
+    }, {
+      item_name: 'Meal', //Meal, Drink
+      item_id: 'IN-FLIGHT DINNING',
+      price: 15.25,
+      item_category: 'Sandwich',
+      item_list_id: 'PDML',
+      quantity: 1
+    }, {
+      item_name: 'Perfume',
+      item_id: 'DUTY-FREE PRODUCT',
+      price: 15.25,
+      item_category: 'Duty-Free Product',
+      quantity: 1
+    }, {
+      item_name: 'Movie', //Movie, Wifi
+      item_id: 'In-Flight Entretainment',
+      price: 15.25,
+      item_category: 'Interview with the Vampire',
+      item_list_id: 'PDIE',
+      quantity: 1
+    }, {
+      item_name: 'Airport Parking',
+      item_id: 'AIRPORT PARKING',
+      price: 15.25,
+      item_category: 'Airport Parking',
+      quantity: 1
+    }, {
+      item_name: 'Bus', //Bus, Car, Shuttle, Train
+      item_id: 'GROUND TRANSPORTATION',
+      price: 15.25,
+      item_category: 'Ground Transportation',
+      quantity: 1
+    }, {
+      item_name: 'Animal in Hold',
+      item_id: 'ANIMAL',
+      price: 15.25,
+      item_category: 'Dog',
+      item_list_id: 'AVIH',
+      quantity: 1
+    }, {
+      item_name: 'Lounge', //Lounge, Flight Flex
+      item_id: 'EXTRA',
+      price: 15.25,
+      item_category: 'Longe',
+      item_list_id: 'PDLG',
+      quantity: 1
+    }, {
+      item_name: 'Standby',
+      item_id: 'STANDBY',
+      price: 15.25,
+      item_category: 'Standby',
+      item_list_id: 'PDSB',
+      quantity: 1
+    }, {
+      item_name: 'Unaccompanied Minor', // Unaccompanied Minor, Infant Charge, Pending Passenger
+      item_id: 'PASSENGER CHANGES',
+      price: 15.25,
+      item_category: 'Passenger Charges',
+      item_list_id: 'PDUM',
+      quantity: 1
+    }, {
+      item_name: 'Loyalty Program Credit Card', //Loyalty Program Credit Card, Refund Fee, Pay Layer Fee
+      item_id: 'PAYMENT SERVICES',
+      price: 33.75,
+      item_category: 'American Express',
+      quantity: 1
+    }]
   }
 });
