11d4e9911e5e2422668cfcdbb2f992b50271bf2f
Ruben Alanis <49915647+rubenalanis@users.noreply.github.com>
2022-09-07 12:10:05 -0400

    Create airmodules.event.datalayer.js

---
+++ dataLayer/airmodules.event.datalayer.js
@@ -0,0 +1,37 @@
+dataLayer.push({
+    'event': 'search-initiation',
+    'module': 'em-booking-popup-abstract',
+    'eventAction': 'search-initiation',
+    'actionLabel': null,
+    'tenantCode': 'ETA',
+    'provider': 'Tennis Australia',
+    'eventName': 'Semifinal',
+    'eventLocation': 'Laver Arena',
+    'eventSession': 'Night',
+    'eventExperienceCategory': 'Ticket Only',
+    'eventExperience': 'The Lounge',
+    'eventNameFilter': 'Semifinal',
+    'eventLocationFilter': 'Laver Arena',
+    'eventSessionFilter': 'Night',
+    'eventExperienceCategoryFilter': 'Ticket Only',
+    'eventExperienceFilter': 'The Lounge|Player Pod',
+    'currencyCode': 'LKR',
+    'totalPrice': null,
+    'totalPriceUSD': null,
+    'fareClass': 'ECONOMY',
+    'startDate': '2021-03-13',
+    'endDate': '2021-03-14',
+    'timestamp': '2021-02-16T17:41:43.200Z',
+    'url': 'https: //www.srilankan.com/en-lk/',
+    'passenger': [{
+        'count': 1,
+        'adultCount': 1,
+        'youngAdultCount': null,
+        'childCount': null
+    }],
+    'page': [{
+        'siteEdition': 'en-LK',
+        'countryIsoCode': 'LK',
+        'languageIsoCode': 'en'
+    }]
+});
