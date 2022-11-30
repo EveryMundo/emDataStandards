c23492c909cd65aaee45b6be20ba956473e1c886
Ruben Alanis <49915647+rubenalanis@users.noreply.github.com>
2022-11-29 22:43:23 -0500

    Create em.ecommerce.view_item_list.dataLayer.js

---
+++ dataLayer/em.ecommerce.view_item_list.dataLayer.js
@@ -0,0 +1,170 @@
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+  event: 'view_item_list',
+  dataLayer_version: '1',
+  currency: 'EUR',
+  flight_type: 'ROUND_TRIP',
+  user_id: '731108519', //Loyalty program user ID -> Cross-device Tracking (Optional)
+  ecommerce: {
+    items: [{
+      item_name: 'MIA>PAR',
+      item_id: 'FLIGHT',
+      price: 300.50,
+      item_category: 'First Class', //Fareclass Category
+      item_category2: '2021-11-14', //Flight departure date
+      item_category3: 'EM477', //Flight number(s)
+      item_category4: 'PT3H1M', //Estimated flight duration
+      item_category5: 'Adult', //Passenger type
+      item_variant: 'MIA>JFK>PAR', //Full route
+      quantity: 2
+    }, {
+      item_name: 'PAR>MIA',
+      item_id: 'FLIGHT',
+      price: 299.00,
+      item_category: 'Business Class',
+      item_category2: '2021-11-30', //Flight departure date
+      item_category3: 'EM901', //Flight number(s)
+      item_category4: 'PT3H1M', //Estimated flight duration
+      item_category5: 'Adult', //Passenger type
+      item_variant: 'PAR>JFK>MIA', //Full route
+      quantity: 2
+    }, {
+      item_name: 'First Class', //Lie Flat, Business Class, First Class, Extra Leg Room, Front of Aircraft, Economy
+      item_id: 'SEAT',
+      price: 20.25,
+      item_category: 'Seat',
+      item_list_id: 'SEAT',
+      quantity: 1
+    }, {
+      item_name: 'Hyatt Miami', //Hotel Name
+      item_id: 'HOTEL',
+      price: 15.25,
+      item_category: 'Suite',
+      quantity: 1
+    }, {
+      item_name: 'Apartment', //Apartment, House, Farm
+      item_id: 'PROPERTY RENTAL',
+      price: 15.25,
+      item_category: 'Marquis Residencies',
+      quantity: 1
+    }, {
+      item_name: 'Hertz', //Company Name
+      item_id: 'CAR',
+      price: 15.25,
+      item_category: 'BMW', //Car Model
+      quantity: 1
+    }, {
+      item_name: 'Pricess', //Company Name
+      item_id: 'CRUISE',
+      price: 15.25,
+      item_category: 'Cruise',
+      quantity: 1
+    }, {
+      item_name: 'Checked Bag', //Carry-On, Checked, Overweight, Oversized, Sporting Equipment, Hands-Free, Excess Baggage
+      item_id: 'BAGGAGE',
+      price: 15.25,
+      item_category: '23kg',
+      item_list_id: 'PDBG',
+      quantity: 1
+    }, {
+      item_name: 'Flight Insurance', //Flight, Lost Baggage, Valuable Bag
+      item_id: 'INSURANCE',
+      price: 15.25,
+      item_category: 'Insurance',
+      quantity: 1
+    }, {
+      item_name: 'Miles Purchase', //Paid Membershit, Miles Purchase
+      item_id: 'LOYALTY PROGRAM',
+      price: 15.25,
+      item_category: '5000', //Membership level, Miles Purchased
+      item_list_id: 'PDFF',
+      quantity: 1
+    }, {
+      item_name: 'Fast Track Security', //Priority Boarding, Priority Security, Priority Bag Drop, Priority Check-in, Priority Bag Return
+      item_id: 'PIORITY SERVICE',
+      price: 15.25,
+      item_category: 'Priority Boarding',
+      item_list_id: 'PDFF',
+      quantity: 1
+    }, {
+      item_name: 'Wheel Chair',
+      item_id: 'MEDICAL',
+      price: 15.25,
+      item_category: 'Medical Services',
+      item_list_id: 'WCBD',
+      quantity: 1
+    }, {
+      item_name: 'Snorkeling', //Name of the Activity, Concert, Tour
+      item_id: 'ACTIVITY',
+      price: 15.25,
+      item_category: 'Activity',
+      quantity: 1
+    }, {
+      item_name: 'Meal', //Meal, Drink
+      item_id: 'IN-FLIGHT DINNING',
+      price: 15.25,
+      item_category: 'Sandwich',
+      item_list_id: 'PDML',
+      quantity: 1
+    }, {
+      item_name: 'Perfume',
+      item_id: 'DUTY-FREE PRODUCT',
+      price: 15.25,
+      item_category: 'Duty-Free Product',
+      quantity: 1
+    }, {
+      item_name: 'Movie', //Movie, Wifi
+      item_id: 'IN-FLIGHT ENTRETAINMENT',
+      price: 15.25,
+      item_category: 'Interview with the Vampire',
+      item_list_id: 'PDIE',
+      quantity: 1
+    }, {
+      item_name: 'Airport Parking',
+      item_id: 'AIRPORT PARKING',
+      price: 15.25,
+      item_category: 'Airport Parking',
+      quantity: 1
+    }, {
+      item_name: 'Bus', //Bus, Car, Shuttle, Train
+      item_id: 'GROUND TRANSPORTATION',
+      price: 15.25,
+      item_category: 'Ground Transportation',
+      quantity: 1
+    }, {
+      item_name: 'Animal in Hold',
+      item_id: 'ANIMAL',
+      price: 15.25,
+      item_category: 'Dog',
+      item_list_id: 'AVIH',
+      quantity: 1
+    }, {
+      item_name: 'Lounge', //Lounge, Flight Flex
+      item_id: 'EXTRA',
+      price: 15.25,
+      item_category: 'Lounge',
+      item_list_id: 'PDLG',
+      quantity: 1
+    }, {
+      item_name: 'Standby',
+      item_id: 'STANDBY',
+      price: 15.25,
+      item_category: 'Standby',
+      item_list_id: 'PDSB',
+      quantity: 1
+    }, {
+      item_name: 'Unaccompanied Minor', // Unaccompanied Minor, Infant Charge, Pending Passenger
+      item_id: 'PASSENGER CHANGES',
+      price: 15.25,
+      item_category: 'Passenger Charges',
+      item_list_id: 'PDUM',
+      quantity: 1
+    }, {
+      item_name: 'Loyalty Program Credit Card', //Loyalty Program Credit Card, Refund Fee, Pay Layer Fee
+      item_id: 'PAYMENT SERVICES',
+      price: 33.75,
+      item_category: 'American Express',
+      quantity: 1
+    }]
+  }
+});
